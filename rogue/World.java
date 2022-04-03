31069cfea39b7964d608a605d338ee4f6774a3d6
Johnson Zhou <johnson@simplyuseful.io>
2022-04-03 22:46:01 +1000

    feat: World engine

--- rogue/World.java
+++ rogue/World.java
@@ -1,3 +1,5 @@
+import java.io.Console;
+
 /**
  * The Rogue world map.
  * @author  Johnson Zhou 1302442 <zhoujj@student.unimelb.edu.au>
@@ -16,6 +18,7 @@ public class World {
   private GameCharacter monster;
   private int mapHeight;
   private int mapWidth;
+  private UserConsole console;
 
   public World() {
     this.mapHeight = World.DEFAULT_HEIGHT;
@@ -41,6 +44,13 @@ public class World {
    */
   public void setMonster(GameCharacter monster) {
     this.monster = monster;
+  }
+
+  /**
+   * @param  console - the user console to receive user inputs
+   */
+  public void setConsole(UserConsole console) {
+    this.console = console;
   }
 
   /** private */
@@ -94,21 +104,105 @@ public class World {
     );
   }
 
+  private void movementLoop() throws CharacterCollision{
+    moving : while(true) {
+      this.renderMap();
+      this.console.printPrompt();
+      String command = this.console.readNext();
+      int newX = this.player.getX();
+      int newY = this.player.getY();
+      switch(command) {
+        case "w":
+          newY -= 1;
+          break;
+        case "s":
+          newY += 1;
+          break;
+        case "a":
+          newX -= 1;
+          break;
+        case "d":
+          newX +=1;
+          break;
+        case "home":
+          System.out.println("Returning home...");
+          break moving;
+        default:
+          continue moving;
+      }
+      if (
+        (newX >= 0) && (newX < this.mapWidth) && 
+        (newY >= 0) && (newY < this.mapHeight)
+      ) {
+        this.player.setX(newX);
+        this.player.setY(newY);
+      }
+
+      if (this.checkCollision()) {
+        throw new CharacterCollision(new Battle(this.player, this.monster));
+      }
+    }
+  }
+
   /** public */
 
   /**
    * Starts the game world. Check if player and monster have been 
    * initiated. 
+   * If the player collides with the monster, a CharacterCollision
+   * exception bubbles.
    * @return  false if not ready
    */
-  public boolean start() {
-    if (this.player == null || this.monster == null) { return false; }
-    this.reset();
-    this.renderMap();
+  public void start() throws WorldNotReady, CharacterCollision {
+    if (
+      this.player == null || 
+      this.monster == null || 
+      this.console == null
+    ) {
+      throw new WorldNotReady();
+    }
 
-    //todo  begin movement loop
-    //todo  Console class
-    
-    return true;
+    // reset the player positions
+    this.reset();
+
+    // begin the movement loop
+    try {
+      this.movementLoop();
+    } catch (CharacterCollision c) {
+      // bubble the CharacterCollision and Battle to the GameEngine
+      throw new CharacterCollision(c.getBattle());
+    }
+  }
+}
+
+/** thrown exceptions */
+
+class WorldNotReady extends Exception {
+  public WorldNotReady() {
+    super("World engine not ready");
+  }
+}
+
+/**
+ * To be thrown when two game characters collide and will 
+ * carry a Battle object with the attacker and defender
+ */
+class CharacterCollision extends Exception {
+
+  private Battle battle;
+
+  /**
+   * @param  battle - a battle resulting from the collision
+   */
+  public CharacterCollision(Battle battle) {
+    super("Game characters have collided");
+    this.battle = battle;
+  }
+
+  /**
+   * @return  the battle object
+   */
+  public Battle getBattle() {
+    return this.battle;
   }
 }
