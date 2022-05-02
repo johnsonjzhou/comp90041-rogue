9b8c9a23bd0c3ac1f8fe918fccd24c28b81d2473
Johnson Zhou <johnson@simplyuseful.io>
2022-05-02 21:22:08 +1000

    feat: interface Fightable

---
+++ rogue/Fightable.java
@@ -0,0 +1,41 @@
+/**
+ * Minimum functionality for entities that can engage in fights. 
+ * @author  Johnson Zhou 1302442 <zhoujj@student.unimelb.edu.au>
+ */
+interface Fightable {
+
+  /**
+   * Sets the maximum health value 
+   * @param  maxHealth 
+   */
+  public void setMaxHealth(int maxHealth);
+
+  /**
+   * Restores the health value back to maximum 
+   */
+  public void restoreHealth();
+
+  /**
+   * Sets the damage value that can be applied to others 
+   * @param  damage 
+   */
+  public void setDamage(int damage);
+
+  /**
+   * Gets the previously set damage value 
+   * @return  damage value as int 
+   */
+  public int getDamage();
+
+  /**
+   * Attacks another Fightable object 
+   */
+  public boolean attacks(Fightable foe);
+
+  /**
+   * Reduces the health value of self by applying the specified damage value 
+   * @param  damage 
+   */
+  public int receiveDamage(int damage); 
+
+}
