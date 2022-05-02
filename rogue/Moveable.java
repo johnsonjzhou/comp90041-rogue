526c96f134da3cc335085db3168e89bc4b1ddb77
Johnson Zhou <johnson@simplyuseful.io>
2022-05-02 21:01:22 +1000

    feat: split position to X, Y

--- rogue/Moveable.java
+++ rogue/Moveable.java
@@ -1,9 +1,14 @@
 interface Moveable {
 
   /**
-   * Gets the current position 
+   * Gets the current X position 
    */
-  public int[] getPosition(); 
+  public int getX(); 
+
+  /**
+   * Gets the current Y position 
+   */
+  public int getY();
 
   /**
    * Initiate a movement command 
