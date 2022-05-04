07fdc9a9f9cb4278c70e979cc9ba7adacc4c43b7
Johnson Zhou <johnson@simplyuseful.io>
2022-05-04 14:01:59 +0800

    feat: Terrain class

---
+++ Terrain.java
@@ -0,0 +1,40 @@
+/**
+ * Terrain entity for the world map 
+ * @author  Johnson Zhou 1302442 <zhoujj@student.unimelb.edu.au>
+ *
+ */
+public class Terrain extends Entity {
+
+  public static enum Types { GROUND, MOUNTAIN, WATER };
+
+  private Types type; 
+
+  public Terrain() {
+    this.type = Types.GROUND;
+  }
+  
+  public Terrain(Types type) {
+    this.type = type;
+  }
+
+  /** getter */
+
+  public boolean isPassable() {
+    switch(this.type) {
+      case MOUNTAIN: return false;
+      case WATER: return false;
+      case GROUND: default: return true;
+    }
+  }
+
+  /** Entity */
+
+  @Override
+  public char getMapMarker() {
+    switch(this.type) {
+      case MOUNTAIN: return '#';
+      case WATER: return '~';
+      case GROUND: default: return '.';
+    }
+  }
+}
