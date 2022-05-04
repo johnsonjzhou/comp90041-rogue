686b35d147c4428af35a8ffa5407e53ebe9f7853
Johnson Zhou <johnson@simplyuseful.io>
2022-05-04 12:03:41 +0800

    feat: assignment template files

---
+++ template/Player.java
@@ -0,0 +1,2 @@
+
+// TODO: Copy code from your Assignment 1 as starter code.
---
+++ template/TestHarness.java
@@ -0,0 +1,12 @@
+
+public class TestHarness {
+
+	public static void main(String[] args) {
+		
+		// TODO: Use this as a testing playground to test the different parts of your code.
+		// The code in this class will not be assessed.
+		
+		
+	}
+	
+}
