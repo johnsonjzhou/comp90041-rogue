f0c9b1da7b4e9ed1fb08a25fd5534db4367c4cb0
Johnson Zhou <johnson@simplyuseful.io>
2022-05-09 11:33:32 +0800

    feat(ioexceptions): thrown causes

--- IOExceptions.java
+++ IOExceptions.java
@@ -3,7 +3,14 @@
  * @author  Johnson Zhou 1302442 <zhoujj@student.unimelb.edu.au>
  */
 public class IOExceptions extends Exception {
+
+  public static final String GEN_MESSAGE = "An error occurred while loading the file.";
+
   public IOExceptions() {
-    super("An error occurred while loading the file.");
+    super(IOExceptions.GEN_MESSAGE);
+  }
+
+  public IOExceptions(String message) {
+    super(IOExceptions.GEN_MESSAGE, new Throwable(message));
   }
 }
