1cf5e47f7b0fbea65391fc275ef8d7238d43b4bb
Johnson Zhou <johnson@simplyuseful.io>
2022-05-09 19:09:13 +0800

    feat(monster): load method

--- Monster.java
+++ Monster.java
@@ -13,6 +13,72 @@ public class Monster extends GameCharacter {
     this.setType("Monster");
   }
 
+  /**
+   * Short cut constructor at also invokes the <code>load</code> method. 
+   * @param  loadString  matching format 
+   *                     <code>monster x y monsterName health attack</code> 
+   * @param  map  the world map 
+   * @throws  IOExceptions  if loadString does not contain valid data 
+   */
+  public Monster(String loadString, Map map) throws IOExceptions {
+    // init GameCharacter
+    super();
+
+    // init Monster
+    this.setType("Monster");
+
+    this.load(loadString, map);
+  }
+
+  /**
+   * todo Loads Monster information from a string of text 
+   * @param  loadString  matching format 
+   *                     <code>monster x y monsterName health attack</code> 
+   * @param  map  the world map 
+   * @throws  IOExceptions  if loadString does not contain valid data 
+   */
+  public void load(String loadString, Map map) throws IOExceptions {
+    String[] loaded = loadString.split(" ");
+
+    // validate load string length 
+    if (loaded.length != 6) {
+      throw new IOExceptions("Unexpected length of monster load string");
+    }
+
+    // validate string identifier 
+    if (!loaded[0].equals("monster")) {
+      throw new IOExceptions("'monster' identifier expected in loadString");
+    }
+
+    // parse monster coordinates 
+    int x = -1;
+    int y = -1;
+    try {
+      x = Integer.parseInt(loaded[1]);
+      y = Integer.parseInt(loaded[2]);
+    } catch (NumberFormatException e) {
+      throw new IOExceptions("Could not parse monster coordinates");
+    }
+
+    // set monster coordinates 
+    if (!map.traversable(x, y) || x < 0 || y < 0) {
+      throw new IOExceptions("Non-traversable monster coordinates");
+    }
+
+    this.setX(x);
+    this.setY(y);
+
+    // parse and set monster name and attributes 
+    try {
+      String name = loaded[3];
+      int maxHealth = Integer.parseInt(loaded[4]);
+      int damage = Integer.parseInt(loaded[5]);
+      this.create(name, maxHealth, damage, true);
+    } catch (NumberFormatException e) {
+      throw new IOExceptions("Could not parse monster attributes");
+    }
+  }
+
   /** Entity */
 
   /**
